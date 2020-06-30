67ca223f0abc81161aed7ee47409bc3f31aa26f3
i1red <ivankras67@gmail.com>
2020-07-01 00:55:41 +0300

    Added settings file

---
+++ lab2/built/settings.js
@@ -0,0 +1,12 @@
+export var StyleClass;
+(function (StyleClass) {
+    StyleClass["Array"] = "array";
+    StyleClass["DivArrayItem"] = "arrayItem";
+    StyleClass["CurrentItem"] = "currentItem";
+    StyleClass["SwappedItem"] = "swappedItem";
+    StyleClass["MinItem"] = "minItem";
+    StyleClass["TmpItem"] = "tmpItem";
+    StyleClass["PartitionResultItem"] = "partitionResultItem";
+    StyleClass["PartitionCurrentItem"] = "currentPartitionItem";
+    StyleClass["PivotItem"] = "pivotItem";
+})(StyleClass || (StyleClass = {}));
---
+++ lab2/src/settings.ts
@@ -0,0 +1,11 @@
+export enum StyleClass {
+    Array = 'array',
+    DivArrayItem = 'arrayItem',
+    CurrentItem = 'currentItem',
+    SwappedItem = 'swappedItem',
+    MinItem = 'minItem',
+    TmpItem = 'tmpItem',
+    PartitionResultItem = 'partitionResultItem',
+    PartitionCurrentItem = 'currentPartitionItem',
+    PivotItem = 'pivotItem',
+}
